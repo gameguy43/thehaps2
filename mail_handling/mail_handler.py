fff6f1663f1a1eb4f7a25ddd200667512907e7df
Parker Phinney <parker.phinney@gmail.com>
2012-01-28 22:39:59 -0500

    modified email handler to do less.

--- mail_handling/mail_handler.py
+++ mail_handling/mail_handler.py
@@ -1,11 +1,35 @@
 import sys
 from email.parser import Parser
+import urllib
 
 #process_from = 'stdin'
 process_from = 'file'
 in_file = '/tmp/sample_msg.email'
 
+destination_url = 'http://calendaritem.com/add/email'
+
+def lightweight_handler():
+    '''send the email to the site as plaintext in POST data'''
+    # grab a file pointer for our email
+    if process_from == 'stdin':
+        fp = sys.stdin
+    elif process_from == 'file':
+        fp = open(in_file, 'r')
+    else:
+        print "something terrible has happened"
+        return
+    # get the email as a string
+    email_str = fp.read()
+    post_data = {
+        'email_str' : email_str,
+        }
+    fp = urllib.urlopen(destination_url,
+                        urllib.urlencode(post_data))
+    #print fp.read() #DEBUG
+
+
 def main():
+    '''this does a bit more processing of the data. but we should move the processing code to the main server. the goal of this file is just to convert an email into a post request in real time'''
     # grab a file pointer for our email
     if process_from == 'stdin':
         fp = sys.stdin
@@ -23,4 +47,4 @@ def just_save_email():
     f = open('/tmp/outfile.txt', 'w')
     f.write(in_text)
 
-main()
+lightweight_handler()
