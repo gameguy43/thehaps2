ed156cc1b5dadac7b7a58a1c63bbddb7a89feb0b
Parker Phinney <parker.phinney@gmail.com>
2012-02-07 01:51:27 -0500

    bugfix: added import datetime to models

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+import datetime
 
 class CalendarItem(models.Model):
     name = models.CharField(max_length=100)
