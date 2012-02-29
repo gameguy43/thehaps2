b7f4958037dfff8220d5db858e9796370758d293
Parker Phinney <parker.phinney@gmail.com>
2012-02-29 03:50:20 -0500

    made mail handler much smarter. this rules

--- mail_handling/mail_handler.py
+++ mail_handling/mail_handler.py
@@ -1,49 +1,39 @@
 import sys
-from email.parser import Parser
 import urllib
 import datetime
-
-
-#JUST_SAVE_EMAIL = True
-JUST_SAVE_EMAIL = False
-
-process_from = 'stdin'
-#process_from = 'file'
-in_file = './sample_msg.email'
+from optparse import OptionParser
 
 destination_url = 'http://calendaritem.com/add/email'
 
-def lightweight_handler():
-    '''send the email to the site as plaintext in POST data'''
+parser = OptionParser()
+# "log" or "only_server_response"
+parser.add_option("-o", "--output", action="store", type="string", dest="output", default="log", help="'log' or 'only_server_response'")
+(options, args) = parser.parse_args()
+
+if options.output == 'log':
     print "======================================"
-    print "datetime::"
+    print "datetime:"
     print datetime.datetime.now()
-    # grab a file pointer for our email
-    if process_from == 'stdin':
-        fp = sys.stdin
-    elif process_from == 'file':
-        fp = open(in_file, 'r')
-    else:
-        print "something terrible has happened"
-        return
-    # get the email as a string
-    email_str = fp.read()
+
+# grab a file pointer for our email
+fp = sys.stdin
+# get the email as a string
+email_str = fp.read()
+
+if options.output == 'log':
     print "email str:"
     print email_str
-    post_data = {
-        'email_str' : email_str,
-        }
-    fp = urllib.urlopen(destination_url,
-                        urllib.urlencode(post_data))
-    output = fp.read()
+
+post_data = {
+    'email_str' : email_str,
+    }
+fp = urllib.urlopen(destination_url,
+                    urllib.urlencode(post_data))
+output = fp.read()
+
+if options.output == 'log':
     print "server response:"
     print output
     print "-----------------------"
-    return output
-
-if JUST_SAVE_EMAIL:
-    import just_save_email
-    just_save_email.just_save_email()
-else:
-    output = lightweight_handler()
+elif options.output == 'only_server_response':
     print output
