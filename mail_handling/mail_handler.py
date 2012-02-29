062f47bc3236f64a4a4b97879331133db8a413f6
Parker Phinney <parker.phinney@gmail.com>
2012-02-29 03:56:14 -0500

    can specify remote or local in mail handler flags

--- mail_handling/mail_handler.py
+++ mail_handling/mail_handler.py
@@ -3,12 +3,22 @@ import urllib
 import datetime
 from optparse import OptionParser
 
-destination_url = 'http://calendaritem.com/add/email'
+remote_url = 'http://calendaritem.com/add/email'
+local_url = "http://localhost:8000/add/email"
 
 parser = OptionParser()
 # "log" or "only_server_response"
 parser.add_option("-o", "--output", action="store", type="string", dest="output", default="log", help="'log' or 'only_server_response'")
+# "default" or "local"
+parser.add_option("-d", "--destination_url", action="store", type="string", dest="dest_url", default="log", help="where to send the email as post? options are hard-coded in this file. so do one of 'default' or 'local'")
 (options, args) = parser.parse_args()
+
+if options.dest_url == 'default':
+    destination_url = remote_url
+elif options.dest_url == 'local':
+    destination_url = local_url
+else:
+    destination_url = remote_url
 
 if options.output == 'log':
     print "======================================"
