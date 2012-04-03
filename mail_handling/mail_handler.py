eda6dbf26c5009f54bae556041054f6c20f81bc9
Parker Phinney <parker.phinney@gmail.com>
2012-04-03 16:05:23 -0400

    adjusted mail handler to send response email in case of error from the webserver

--- mail_handling/mail_handler.py
+++ mail_handling/mail_handler.py
@@ -2,6 +2,10 @@ import sys
 import urllib
 import datetime
 from optparse import OptionParser
+import StringIO
+
+
+SUCCESS_OUTPUT = '1'
 
 remote_url = 'http://calendaritem.com/add/email'
 local_url = "http://localhost:8000/add/email"
@@ -47,3 +51,29 @@ if options.output == 'log':
     print "-----------------------"
 elif options.output == 'only_server_response':
     print output
+
+if output != SUCCESS_OUTPUT:
+    from email.parser import Parser as emailParser
+    import email.utils
+    import smtplib
+    from email.mime.text import MIMEText
+
+    # get their email address
+    parsed_email = emailParser().parse(StringIO.StringIO(email_str))
+    real_name, email_addr = email.utils.parseaddr(parsed_email['From'])
+
+    # email them the output
+    # assemble the email
+    to_addr = email_addr
+    from_addr = 'robot@calendaritem.com'
+    email_strs = ['CalendarItem failed to process your email. Something went horribly wrong. This is super lame and we\'re real sorry about it. Here\'s what the server said:\n\n']
+    email_strs.append(output)
+    msg = MIMEText('\n'.join(email_strs))
+    msg['Subject'] = '[CalendarItem] ERROR! OH NO!'
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+
+    # send it
+    s = smtplib.SMTP('localhost')
+    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.quit()
